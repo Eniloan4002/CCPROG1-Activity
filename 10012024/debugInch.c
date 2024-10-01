b9b5b0fbfedd488cc344825ce368113d21a53069
Eniloan4002 <140362869+Eniloan4002@users.noreply.github.com>
2024-10-01 10:38:52 +0800

    Update debugInch.c

--- 10012024/debugInch.c
+++ 10012024/debugInch.c
@@ -2,22 +2,24 @@
 int main()
 
 {
-int   inch;
-float feet, yard, cm;                   //formerly ',' instead o;
-
-printf("Enter value in inches: ");
-    scanf("%d", &inch);                 //formerly scanf("%d, inch");
+    //Declaring variables
+    int   inch;
+    float feet, yard, cm;                   //formerly ',' instead o;
     
-feet = inch / 12.0;                     //
-yard = feet / 3;                        //formerly yard = yard / 3
-cm = inch * 2.54;                       //formerly 'x' instead of *
-                                        //and missing ';'
-printf("\n%d inches is equivalent to:\n", inch);
-printf("Feet = %.2f\n", feet);
-printf("Yard = %.2f\n", yard);
-printf("CM = %.2f\n", cm);              //formerly '%2f\n'
-    
-//potentially irrelevant
-printf("M = CM / 100\n");               //formerly 'cm'
-return 0;
+    //asking user inpute
+    printf("Enter value in inches: ");
+        scanf("%d", &inch);                 //formerly scanf("%d, inch");
+        
+    feet = inch / 12.0;                     //
+    yard = feet / 3;                        //formerly yard = yard / 3
+    cm = inch * 2.54;                       //formerly 'x' instead of *
+                                            //and missing ';'
+    printf("\n%d inches is equivalent to:\n", inch);
+    printf("Feet = %.2f\n", feet);
+    printf("Yard = %.2f\n", yard);
+    printf("CM = %.2f\n", cm);              //formerly '%2f\n'
+        
+    //potentially irrelevant
+    printf("M = CM / 100\n");               //formerly 'cm'
+    return 0;
 }
