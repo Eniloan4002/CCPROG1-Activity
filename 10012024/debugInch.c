494f4873137e940a31cb010d6287522089ab5ae6
Eniloan4002 <140362869+Eniloan4002@users.noreply.github.com>
2024-10-01 10:39:48 +0800

    Update debugInch.c

--- 10012024/debugInch.c
+++ 10012024/debugInch.c
@@ -9,8 +9,9 @@ int main()
     //asking user inpute
     printf("Enter value in inches: ");
         scanf("%d", &inch);                 //formerly scanf("%d, inch");
-        
-    feet = inch / 12.0;                     //
+
+    //Converting inches into different measurements
+    feet = inch / 12.0;                     
     yard = feet / 3;                        //formerly yard = yard / 3
     cm = inch * 2.54;                       //formerly 'x' instead of *
                                             //and missing ';'
